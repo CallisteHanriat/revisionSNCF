085b1e2b4a9dacc00a69ea7bd800611927be7378
CallisteHanriat <couim38@gmail.com>
2015-11-04 20:28:48 +0100

    initial commit

---
+++ revSncf/main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: loft-2015-asus
+ *
+ * Created on 4 novembre 2015, 20:27
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
